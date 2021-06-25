12fb591c58a129582a41cfc08a5e6ffe4fa780e3
rajasgandhi <rajasmgandhi@gmail.com>
2021-06-24 19:03:43 -0500

    inital commit

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -1,30 +1,47 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
+
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+#include <iostream>
+#include <fstream>
+
 using namespace llvm;
+//using namespace std;
 
-namespace {
-  struct SkeletonPass : public FunctionPass {
+namespace
+{
+  struct Hello : public FunctionPass
+  {
     static char ID;
-    SkeletonPass() : FunctionPass(ID) {}
+    Hello() : FunctionPass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-      errs() << "I saw a function called " << F.getName() << "!\n";
+    bool runOnFunction(Function &F) override
+    {
+      errs() << F.getName() << ": " << F.getInstructionCount() << "\n";
+      /*for (BasicBlock *BB : F)
+      {
+        errs() << "BB has name" << BB->getName() << "\n";
+      }*/
+      for (Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb)
+      {
+        errs() << "BB has name" << bb->getName() << "\n";
+
+      }
       return false;
     }
-  };
-}
+  }; // end of struct Hello
+} // end of anonymous namespace
 
-char SkeletonPass::ID = 0;
+char Hello::ID = 0;
+static RegisterPass<Hello> X("hello", "Hello World Pass",
+                             false /* Only looks at CFG */,
+                             false /* Analysis Pass */);
 
-// Automatically enable the pass.
-// http://adriansampson.net/blog/clangpass.html
-static void registerSkeletonPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
-  PM.add(new SkeletonPass());
-}
-static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerSkeletonPass);
+static RegisterStandardPasses Y(
+    PassManagerBuilder::EP_EarlyAsPossible,
+    [](const PassManagerBuilder &Builder,
+       legacy::PassManagerBase &PM)
+    { PM.add(new Hello()); });
